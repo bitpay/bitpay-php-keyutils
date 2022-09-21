ec92cc7016d071a0b3f3b4cef2c48bbf8bcf0fab
therevix <EH88qbdm3dj9xdULjAXm>
2022-09-21 11:37:48 +0200

    SP-216 - Unit Tests - FilesystemStorage - part

--- test/unit/BitPayKeyUtils/Storage/FilesystemStorageTest.php
+++ test/unit/BitPayKeyUtils/Storage/FilesystemStorageTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use BitPayKeyUtils\KeyHelper\Key;
 use BitPayKeyUtils\Storage\FilesystemStorage;
 use PHPUnit\Framework\TestCase;
 
@@ -10,6 +11,42 @@ class FilesystemStorageTest extends TestCase
         $filesystemStorage = $this->createClassObject();
 
         $this->assertInstanceOf(FilesystemStorage::class, $filesystemStorage);
+    }
+
+    public function testPersist()
+    {
+        $filesystemStorage = $this->createClassObject();
+        $keyInterface = $this->getMockBuilder(Key::class)->getMock();
+        $keyInterface->method('getId')->willReturn(__DIR__ . '/test1.txt');
+        $this->assertFileExists(__DIR__ . '/test1.txt');
+        @chmod(__DIR__ . 'test1.txt', 0777);
+        $this->assertEquals(null, $filesystemStorage->persist($keyInterface));
+    }
+
+    public function testLoadNotFindException()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Could not find "'.__DIR__.'/test2.txt"');
+
+        $filesystemStorage = $this->createClassObject();
+        $filesystemStorage->load(__DIR__ . '/test2.txt');
+    }
+
+    protected function tearDown(): void
+    {
+        @chmod(__DIR__ . '/test3.txt', 0755);
+    }
+
+    public function testLoadNotPermissionException()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('"test3.txt" cannot be read, check permissions');
+
+        @chmod(__DIR__ . '/test3.txt', 0755);
+        $this->tearDown();
+
+        $filesystemStorage = $this->createClassObject();
+        $filesystemStorage->load(__DIR__ . '/test3.txt');
     }
 
     private function createClassObject()
