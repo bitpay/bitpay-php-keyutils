1836f9a3e3b3d63b76ba5d997de02ae1e60be58f
therevix <EH88qbdm3dj9xdULjAXm>
2022-09-27 13:11:00 +0200

    SP-216 - Unit Tests - FileSystemStorage

--- test/unit/BitPayKeyUtils/Storage/FilesystemStorageTest.php
+++ test/unit/BitPayKeyUtils/Storage/FilesystemStorageTest.php
@@ -32,18 +32,12 @@ class FilesystemStorageTest extends TestCase
         $filesystemStorage->load(__DIR__ . '/test2.txt');
     }
 
-    protected function tearDown(): void
-    {
-        @chmod(__DIR__ . '/test3.txt', 0755);
-    }
-
     public function testLoadNotPermissionException()
     {
         $this->expectException(Exception::class);
         $this->expectExceptionMessage('"test3.txt" cannot be read, check permissions');
 
         @chmod(__DIR__ . '/test3.txt', 0755);
-        $this->tearDown();
 
         $filesystemStorage = $this->createClassObject();
         $filesystemStorage->load(__DIR__ . '/test3.txt');
